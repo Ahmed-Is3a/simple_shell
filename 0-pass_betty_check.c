4b89c6cb3d4f40850028d76b853499db13edcb18
ahmed <ahmedissaten@gmail.com>
2023-11-04 14:39:18 +0200

    passing betty check

---
+++ 0-pass_betty_check.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -  a beautiful code that passes the Betty checks
+ *
+ * Return: 0 (on success)
+*/
+int main(void)
+{
+	printf("Betty check passed!\n");
+	return (0);
+}
