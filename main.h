01e9f8e74421b29ad320383f356d08cb3da32f4b
ahmed <ahmedissaten@gmail.com>
2023-11-13 23:30:19 +0200

    handle mulipule commands seperated with ;

--- main.h
+++ main.h
@@ -4,10 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
 #define TOK_DELIMITERS " \t\r\n\a\""
+#define MAX_COMMANDS 10
+#define MAX_COMMAND_LENGTH 100
 
 int pass_betty_check(void);
+int execute_args(char *command, char *shell);
+void handle_multi_commands(char *input, char *shell);
 void display_shell_prompt(char *shell);
 
 #endif
